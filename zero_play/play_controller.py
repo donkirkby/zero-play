9d0d627404e40ee861657f9cc422f966473133b0
Don Kirkby <donkirkby@gmail.com>
2020-11-14 17:23:10 -0800

    Allow different player colours in PlayController.

--- zero_play/play_controller.py
+++ zero_play/play_controller.py
@@ -26,10 +26,10 @@ class PlayController:
         x_player: Player
         o_player: Player
         x_player, o_player = players
-        x_player.player_number = start_state.X_PLAYER
-        o_player.player_number = start_state.O_PLAYER
-        self.players = {start_state.X_PLAYER: x_player,
-                        start_state.O_PLAYER: o_player}
+        x_player.player_number = start_state.players[0]
+        o_player.player_number = start_state.players[1]
+        self.players = {x_player.player_number: x_player,
+                        o_player.player_number: o_player}
         self.results = [PlayerResults(x_player), PlayerResults(o_player)]
         self.start_game()
 
@@ -50,7 +50,10 @@ class PlayController:
         if not self.board.is_ended():
             return False
 
-        other_player = self.players[-player_number]
+        other_player = None
+        for other_player in self.players.values():
+            if other_player is not player:
+                break
         player.end_game(self.board, other_player)
         other_player.end_game(self.board, player)
 
@@ -63,18 +66,20 @@ class PlayController:
         raise ValueError('Player not found.')
 
     def play(self, games: int = 1, flip: bool = False, display: bool = False):
-        current_x = original_x = self.players[self.start_state.X_PLAYER]
-        current_o = original_o = self.players[self.start_state.O_PLAYER]
+        x_number = self.start_state.players[0]
+        o_number = self.start_state.players[1]
+        current_x = original_x = self.players[x_number]
+        current_o = original_o = self.players[o_number]
         self.results = [PlayerResults(current_x), PlayerResults(current_o)]
         ties = 0
         for i in range(games):
             if i and flip:
-                current_x = self.players[self.start_state.O_PLAYER]
-                current_o = self.players[self.start_state.X_PLAYER]
-                current_x.player_number = self.start_state.X_PLAYER
-                current_o.player_number = self.start_state.O_PLAYER
-                self.players[self.start_state.X_PLAYER] = current_x
-                self.players[self.start_state.O_PLAYER] = current_o
+                current_x = self.players[o_number]
+                current_o = self.players[x_number]
+                current_x.player_number = x_number
+                current_o.player_number = o_number
+                self.players[x_number] = current_x
+                self.players[o_number] = current_o
             while True:
                 if display:
                     print(self.board.display(show_coordinates=True))
@@ -82,17 +87,17 @@ class PlayController:
                     break
             if display:
                 print(self.board.display(show_coordinates=True))
-            if self.board.is_win(self.board.X_PLAYER):
+            if self.board.is_win(x_number):
                 self.get_player_results(current_x).win_count += 1
-            elif self.board.is_win(self.board.O_PLAYER):
+            elif self.board.is_win(o_number):
                 self.get_player_results(current_o).win_count += 1
             else:
                 ties += 1
             self.start_game()
-        original_x.player_number = self.board.X_PLAYER
-        original_o.player_number = self.board.O_PLAYER
-        self.players[self.board.X_PLAYER] = original_x
-        self.players[self.board.O_PLAYER] = original_o
+        original_x.player_number = x_number
+        original_o.player_number = o_number
+        self.players[x_number] = original_x
+        self.players[o_number] = original_o
         for player_results in self.results:
             print(player_results.get_summary())
         print(ties, 'ties')
